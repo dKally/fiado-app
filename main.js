ed4f16fa252372d6f4bd9740a3c79b899a0b7729
dKally <110414111+dKally@users.noreply.github.com>
2023-09-16 13:18:06 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-const {app, BrowserWindow, ipcMain, Menu} = require('electron')
+const {app, BrowserWindow, ipcMain, Menu, globalShortcut} = require('electron')
 const path = require('path')
 
 let win
 
 const isDev = process.env.NODE_ENV !== undefined && process.env.NODE_ENV === "development"? true:false
 
+app.setName("FiadoAPP")
+
 function createWindow(){
     win = new BrowserWindow({
         width: 1920,
         height: 1080,
+        icon: path.join(__dirname, 'icon.png'),
         fullscreen: true,
         show: false,
         webPreferences:{
@@ -27,12 +30,16 @@ function createWindow(){
     })
 
     ipcMain.on('redirect-to-client', () => {
-        const newPath = path.join(__dirname, 'client.html');
-        win.loadFile(newPath);
+        const newPath = path.join(__dirname, 'client.html')
+        win.loadFile(newPath)
     })
 
-    const mainMenu = Menu.buildFromTemplate(menuTemplate);
-    Menu.setApplicationMenu(mainMenu);
+    const mainMenu = Menu.buildFromTemplate(menuTemplate)
+    Menu.setApplicationMenu(mainMenu)
+
+    globalShortcut.register('Shift+I', () => {
+        win.webContents.openDevTools()
+      });
 }
 
 app.whenReady().then(()=>{
@@ -49,6 +56,15 @@ app.on('activate', ()=>{
         createWindow()
     }
 })
+
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+  });
+  
+  app.on('will-quit', function () {
+    globalShortcut.unregisterAll();
+  });
 
 const menuTemplate = [
 
