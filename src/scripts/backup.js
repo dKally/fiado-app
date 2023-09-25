3351f3ed37367b149664b1e7a4e1002234b14b19
dKally <110414111+dKally@users.noreply.github.com>
2023-09-25 15:15:38 -0300

    Add files via upload

--- src/scripts/backup.js
+++ src/scripts/backup.js
@@ -1,8 +1,11 @@
-const fs = require('fs')
+const fs = require('fs-extra');
 const zip = require('zip-local')
 const path = require('path')
 const os = require('os')
 const { shell } = require('electron')
+const { ipcRenderer } = require('electron')
+
+
 
 document.querySelector('.save-backup').addEventListener('click', ()=>{
     saveBackup()
@@ -10,6 +13,10 @@ document.querySelector('.save-backup').addEventListener('click', ()=>{
 
 document.querySelector('.open-backup').addEventListener('click', ()=>{
     openBackupPaste()
+})
+
+document.querySelector('.backup').addEventListener('click', ()=>{
+    openBackup()
 })
 
 function saveBackup() {
@@ -26,3 +33,32 @@ function saveBackup() {
 function openBackupPaste(){
     shell.openPath(path.join(os.homedir(), 'Documentos'))
 }
+
+
+function openBackup() {
+  ipcRenderer.send('open-dialog');
+}
+
+// Para receber a pasta selecionada do processo principal
+ipcRenderer.on('selected-folder', (event, paths) => {
+    if (!paths || paths.length === 0) {
+      console.log('Seleção de pasta cancelada.');
+    } else {
+      const folderPath = paths[0];
+      const destinoPasta = path.join(os.homedir(), 'Documentos', 'Clientes FiadoAPP');
+  
+      // Verifique o nome da pasta e mova, substituindo se já existir
+      if (path.basename(folderPath) === 'Clientes FiadoAPP') {
+        try {
+          fs.removeSync(destinoPasta); // Remova o destino existente recursivamente
+          fs.moveSync(folderPath, destinoPasta); // Mova a nova pasta para o destino
+  
+          console.log(`A pasta "Clientes FiadoAPP" foi movida e substituiu ${destinoPasta}`);
+        } catch (error) {
+          console.error('Erro ao mover a pasta:', error);
+        }
+      } else {
+        console.log('O nome da pasta selecionada não é "Clientes FiadoAPP".');
+      }
+    }
+  });
